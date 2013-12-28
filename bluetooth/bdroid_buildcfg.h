040dd702be86dca797431b1407da1e13d6ce8498
Darescu Ionut <ionut.darescu@gmail.com>
2013-12-28 23:08:46 +0200

    shooteru: KitKat bringup

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,4 +20,12 @@
 #define BTM_DEF_LOCAL_NAME   "HTC Evo 3D"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
+#define A2DP_HW_SYSFS_TUNER "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
+#define A2DP_HW_SYSFS_TUNER_OFF "0"
+#define A2DP_HW_SYSFS_TUNER_ON "432000"
+
+#define BLE_INCLUDED TRUE
+#define BTA_GATT_INCLUDED TRUE
+#define SMP_INCLUDED TRUE
+
 #endif
--- shooteru.mk
+++ shooteru.mk
@@ -30,12 +30,14 @@ PRODUCT_COPY_FILES += \
     device/htc/shooteru/ramdisk/fstab.shooteru:root/fstab.shooteru
 
 ## recovery and custom charging
-PRODUCT_COPY_FILES += \
-    device/htc/shooteru/recovery/sbin/choice_fn:recovery/root/sbin/choice_fn \
-    device/htc/shooteru/recovery/sbin/power_test:recovery/root/sbin/power_test \
-    device/htc/shooteru/recovery/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
-    device/htc/shooteru/recovery/sbin/detect_key:recovery/root/sbin/detect_key \
-    device/htc/shooteru/recovery/sbin/htcbatt:recovery/root/sbin/htcbatt
+PRODUCT_PACKAGES += \
+    init.recovery.shooteru.rc \
+    twrp.fstab \
+    choice_fn \
+    detect_key \
+    offmode_charging \
+    power_test \
+    htcbatt
 
 ## dsp Audio
 PRODUCT_COPY_FILES += \
@@ -167,7 +169,7 @@ $(call inherit-product, device/htc/shooteru/media_htcaudio.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := shooteru
+PRODUCT_NAME := full_shooteru
 PRODUCT_DEVICE := shooteru
 PRODUCT_BRAND := HTC
 PRODUCT_MANUFACTURER := HTC
